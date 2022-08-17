95309d772ccbc416420513309aacd3fe6ca814dd
hemanth9113 <hemanthyash1517@gmail.com>
2022-08-17 10:22:58 -0700

    added build version

---
+++ build/precache-manifest.307ad60d28cd2ee5c087697a1fa6cc6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c54ae23b2e5d4e800629157df8107bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4973acc05c97b0b0544",
+    "url": "/static/css/main.3922d42c.chunk.css"
+  },
+  {
+    "revision": "3567b60a172c4695a6b8",
+    "url": "/static/js/2.dfa222ac.chunk.js"
+  },
+  {
+    "revision": "f7d13dfba7c11f9ea8f09ce8fb2163d2",
+    "url": "/static/js/2.dfa222ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4973acc05c97b0b0544",
+    "url": "/static/js/main.66e70a0c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.66e70a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
