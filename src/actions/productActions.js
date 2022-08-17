8616d8538c0deefc6cd17352964f15b4c6d898fb
hemanth9113 <hemanthyash1517@gmail.com>
2022-08-17 10:14:29 -0700

    added View and logic

---
+++ src/actions/productActions.js
@@ -0,0 +1,150 @@
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_SAVE_REQUEST,
+  PRODUCT_SAVE_SUCCESS,
+  PRODUCT_SAVE_FAIL,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_REVIEW_SAVE_REQUEST,
+  PRODUCT_REVIEW_SAVE_FAIL,
+  PRODUCT_REVIEW_SAVE_SUCCESS,
+  PRODUCT_CATEGORY_FAIL,
+  PRODUCT_CATEGORY_SUCCESS,
+  PRODUCT_CATEGORY_REQUEST
+} from '../constants/productConstants';
+import axios from 'axios';
+import Axios from 'axios';
+
+const listProducts = (
+  category = '',
+  searchKeyword = '',
+  sortOrder = ''
+) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+    const { data } = await axios.get(
+      '/api/products?category=' +
+        category +
+        '&searchKeyword=' +
+        searchKeyword +
+        '&sortOrder=' +
+        sortOrder
+    );
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+  }
+};
+
+const saveProduct = (product) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: PRODUCT_SAVE_REQUEST, payload: product });
+    const {
+      userSignin: { userInfo },
+    } = getState();
+    if (!product._id) {
+      const { data } = await Axios.post('/api/products', product, {
+        headers: {
+          Authorization: 'Bearer ' + userInfo.token,
+        },
+      });
+      dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+    } else {
+      const { data } = await Axios.put(
+        '/api/products/' + product._id,
+        product,
+        {
+          headers: {
+            Authorization: 'Bearer ' + userInfo.token,
+          },
+        }
+      );
+      dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+    }
+  } catch (error) {
+    dispatch({ type: PRODUCT_SAVE_FAIL, payload: error.message });
+  }
+};
+
+const detailsProduct = (productId) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+    const { data } = await axios.get('/api/products/' + productId);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message });
+  }
+};
+
+
+const categoryDetails = (name) => async (dispatch) => {
+  console.log("came herer")
+  try{
+    dispatch({
+      type: PRODUCT_CATEGORY_REQUEST, payload: name
+    })
+     const { data } = await axios.get(`/api/products/category/${name}`);
+
+     console.log("dataaa", data)
+    dispatch({ type: PRODUCT_CATEGORY_SUCCESS, payload: data });
+
+  }catch(err) {
+    dispatch({type: PRODUCT_CATEGORY_FAIL, payload: err.message})
+  }
+}
+
+const deleteProdcut = (productId) => async (dispatch, getState) => {
+  try {
+    const {
+      userSignin: { userInfo },
+    } = getState();
+    dispatch({ type: PRODUCT_DELETE_REQUEST, payload: productId });
+    const { data } = await axios.delete('/api/products/' + productId, {
+      headers: {
+        Authorization: 'Bearer ' + userInfo.token,
+      },
+    });
+    dispatch({ type: PRODUCT_DELETE_SUCCESS, payload: data, success: true });
+  } catch (error) {
+    dispatch({ type: PRODUCT_DELETE_FAIL, payload: error.message });
+  }
+};
+
+const saveProductReview = (productId, review) => async (dispatch, getState) => {
+  try {
+    const {
+      userSignin: {
+        userInfo: { token },
+      },
+    } = getState();
+    dispatch({ type: PRODUCT_REVIEW_SAVE_REQUEST, payload: review });
+    const { data } = await axios.post(
+      `/api/products/${productId}/reviews`,
+      review,
+      {
+        headers: {
+          Authorization: 'Bearer ' + token,
+        },
+      }
+    );
+    dispatch({ type: PRODUCT_REVIEW_SAVE_SUCCESS, payload: data });
+  } catch (error) {
+    // report error
+    dispatch({ type: PRODUCT_REVIEW_SAVE_FAIL, payload: error.message });
+  }
+};
+
+export {
+  listProducts,
+  detailsProduct,
+  saveProduct,
+  deleteProdcut,
+  saveProductReview,
+  categoryDetails
+};
---
+++ src/screens/Category.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios"
+import { Link } from "react-router-dom";
+import Rating from "../components/Rating";
+import { PRODUCT_REVIEW_SAVE_RESET } from "../constants/productConstants";
+
+const Category = (props) => {
+ return null
+ 
+};
+
+export default Category;
